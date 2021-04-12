54501a0184499d809e0f9fcecb85d19d93b6109a
Aleksandr Pinayev <a59-info@yandex.ru>
2021-04-12 14:07:08 +0300

    Задание 3

---
+++ lesson3/lesson3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string stringFromUser = Console.ReadLine();                      
+            char[] charArray = stringFromUser.ToCharArray();
+            //
+            Console.Write("Обратный текст: ");
+            for (int i = stringFromUser.Length - 1; i >= 0; i--) {
+                Console.Write(stringFromUser[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
