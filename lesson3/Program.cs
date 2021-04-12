a2c26b409a45ac3d0521399c6e07f5d882b2678b
Aleksandr Pinayev <a59-info@yandex.ru>
2021-04-12 14:00:22 +0300

    Задание 3

---
+++ lesson3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace homeWork2_lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mynumber;
+            string myanswer;
+
+            do
+            {
+                Console.Write("Введите число: ");
+               mynumber = Int32.Parse(Console.ReadLine());
+                //
+                if (mynumber % 2 == 0)
+                {
+                    // число четное
+                    Console.WriteLine($"{mynumber} - четное");
+                }
+                else
+                {
+                    // число нечетное
+                    Console.WriteLine($"{mynumber} - нечетное");
+                }
+                Console.WriteLine("Выйти из программы? y - да, n - нет");
+                myanswer = Console.ReadLine();
+            } while (myanswer == "n" || myanswer == "N");
+        }
+    }
+}
