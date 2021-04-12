5514acb58976943fd3bb46a9895590821b983cc2
Aleksandr Pinayev <a59-info@yandex.ru>
2021-04-12 13:53:22 +0300

    Задание 1

---
+++ lesson1/homeWork1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace homeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            string username = Console.ReadLine();
+            Console.WriteLine($"Привет, {username}, сегодня {DateTime.Now.ToShortDateString()}");
+            Console.ReadKey();
+        }
+    }
+}
