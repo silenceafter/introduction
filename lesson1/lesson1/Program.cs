033c44fe121ea1237c9f9a3afc58968a462ee70d
Aleksandr Pinayev <a59-info@yandex.ru>
2021-04-12 14:13:32 +0300

    Задание 1

---
+++ lesson1/lesson1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+            Console.Write("Напишите что-нибудь: ");
+            string mytext = Console.ReadLine();
+            Console.Write("Введите имя текстового файла (с расширением .txt): ");
+            string filename = Console.ReadLine();
+            //
+            File.WriteAllText(filename, mytext);
+            Console.WriteLine($"Файл {filename} сохранен");
+        }
+    }
+}
