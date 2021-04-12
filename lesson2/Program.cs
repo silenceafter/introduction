46b0678cce65d8a68daee52e376721e01d9caefa
Aleksandr Pinayev <a59-info@yandex.ru>
2021-04-12 13:58:58 +0300

    Задание 2

---
+++ lesson2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace homeWork2_lesson2
+{
+    class Program
+    {
+        public enum Months
+        {
+            Январь = 1,
+            Февраль,
+            Март,
+            Апрель,
+            Май,
+            Июнь,
+            Июль,
+            Август,
+            Сентябрь,
+            Октябрь,
+            Ноябрь,
+            Декабрь
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите порядковый номер текущего месяца = ");
+            int mymonth = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Это {(Months)mymonth}");
+            Console.ReadKey();
+        }
+    }
+}
